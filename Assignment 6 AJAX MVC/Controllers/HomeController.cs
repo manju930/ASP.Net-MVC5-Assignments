e7e9605e4b13a2a0d1df5fd5583aff9d6f942f11
Manjunath <102520017+manju930@users.noreply.github.com>
2022-05-05 02:18:03 +0000

    Assignment 6 Base version
    code commit

---
+++ Assignment 6 AJAX MVC/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Assignment7Ajax.Models;
+
+namespace Assignment7Ajax.Controllers
+{
+    public class HomeController : Controller
+    {
+        static List<User> users = new List<User>();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetServerTime() //added
+        {
+            System.Threading.Thread.Sleep(5000);
+            return PartialView();
+        }
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult GetAllUsers()
+        {
+            return PartialView(users);
+        }
+        public ActionResult CreateUser() //to create data
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateUser(User u)  //to store data
+        {
+            users.Add(u);
+            return RedirectToAction("GetAllUsers");
+        }
+
+
+
+    }
+}
